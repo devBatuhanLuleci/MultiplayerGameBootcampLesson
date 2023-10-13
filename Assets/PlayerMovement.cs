7a95fab4ad0a42121e621b70344f62f274e986ce
BatuhanPC <learn.batuhanluleci@gmail.com>
2023-10-13 20:02:31 +0300

    13 Ekim 2023 dersinde Input alarak W ile objenin hareket etme sistemi yapıldı.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.05f;
+    
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+           
+            gameObject.GetComponent<Transform>().position += Vector3.forward* speed * Time.fixedDeltaTime;
+
+
+        }
+    }
+
+}
