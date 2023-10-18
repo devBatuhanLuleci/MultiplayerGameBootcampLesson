1719fbd831bfbbd4a8d3f10aaad39b4ba50f4bac
BatuhanPC <learn.batuhanluleci@gmail.com>
2023-10-18 19:05:35 +0300

    coin hareketi düzenleniyor.

---
+++ Assets/CoinMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CoinMovement : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //TODO : SetEase eklenecek ve SetLoops açıklanacak.
+        transform.DOLocalMoveY(1.5f, 1f).SetLoops(-1,LoopType.Yoyo);
+    }
+
+
+
+}
